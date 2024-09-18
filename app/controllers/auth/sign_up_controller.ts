8d70ba5d2459001e674e7cbb044d8c7a55184eab
Guizzs26 <guiteixeira1926@gmail.com>
2024-09-18 18:53:41 -0300

    feat(controller): add signUpController without working yet

--- app/controllers/auth/sign_up_controller.ts
+++ app/controllers/auth/sign_up_controller.ts
@@ -1,9 +1,20 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { signUpUserValidator } from '#validators/auth/sign_up'
+import User from '#models/user'
 
 export default class SignUpController {
-  async signUp({ request, response }: HttpContext) {
-    return response.send({
-      message: 'User created',
+  async signUp({ request, response, auth }: HttpContext) {
+    const { email, password } = await request.validateUsing(signUpUserValidator, {
+      meta: {
+        userId: auth.user!.id,
+      },
+    })
+
+    const user = await User.create({ email, password })
+
+    return response.status(201).send({
+      message: 'User created successfully',
+      user,
     })
   }
 }
